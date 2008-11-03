650ddc121855e78706d30127bf1cdbff7a21ef90
Chris Pickel <sfiera@gmail.com>
2008-11-03 00:59:52 -0500

    Introduce stubs for all missing included files.

---
+++ stubs/AltPreferences.h
@@ -0,0 +1,14 @@
+#ifndef ANTARES_STUB_ALT_PREFERENCES_H_
+#define ANTARES_STUB_ALT_PREFERENCES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+// Content here
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // ANTARES_STUB_ALT_PREFERENCES_H_
---
+++ stubs/GXMath.h
@@ -0,0 +1,14 @@
+#ifndef ANTARES_STUB_GXMATH_H_
+#define ANTARES_STUB_GXMATH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+// Content here
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // ANTARES_STUB_GXMATH_H_
