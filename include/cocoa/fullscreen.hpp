ee6dc9ba82217a9b187fbedb7520e9a28516ebe1
Chris Pickel <sfiera@sfzmail.com>
2012-08-03 01:12:16 -0400

    Update copyright.
    Extend copyright to 2012.  Also, replace "Ares Central" with "The Antares
    Authors", as a reference to the AUTHORS file.  Ares Central isn't really a
    thing; it's the authors who hold copyright.
    
    Also, replace an instance of "this program" with "Antares" in the license.

--- include/cocoa/fullscreen.hpp
+++ include/cocoa/fullscreen.hpp
@@ -1,5 +1,5 @@
 // Copyright (C) 1997, 1999-2001, 2008 Nathan Lamont
-// Copyright (C) 2008-2012 Ares Central
+// Copyright (C) 2008-2012 The Antares Authors
 //
 // This file is part of Antares, a tactical space combat game.
 //
@@ -14,8 +14,7 @@
 // Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public
-// License along with this program.  If not, see
-// <http://www.gnu.org/licenses/>.
+// License along with Antares.  If not, see http://www.gnu.org/licenses/
 
 #ifndef ANTARES_COCOA_FULLSCREEN_HPP_
 #define ANTARES_COCOA_FULLSCREEN_HPP_
