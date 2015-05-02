b189520adea7c505260cec864b3acd9152810e1e
Chris Pickel <sfiera@sfzmail.com>
2015-05-02 18:15:43 +0900

    Make scripts/test.py output pretty.

--- scripts/test.py
+++ scripts/test.py
@@ -8,56 +8,76 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import time
 
 
-def run(name, cmd):
+def run(lock, name, cmd):
     sub = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output, _ = sub.communicate()
     if sub.returncode != 0:
-        sys.stderr.write("%s: %s failed:\n" % (name, os.path.basename(cmd[0])))
-        sys.stderr.write("====================\n")
-        sys.stderr.write(output)
-        sys.stderr.write("====================\n")
+        with lock:
+            sys.stderr.write("%s: %s failed:\n" % (name, os.path.basename(cmd[0])))
+            sys.stderr.write("====================\n")
+            sys.stderr.write(output)
+            sys.stderr.write("====================\n")
         return False
     return True
 
 
-def unit_test(opts, name, args=[]):
-    return run(name, ["out/cur/%s" % name] + args)
+def unit_test(opts, lock, name, args=[]):
+    return run(lock, name, ["out/cur/%s" % name] + args)
 
 
-def diff_test(name, cmd, expected):
+def diff_test(lock, name, cmd, expected):
     with NamedTemporaryDir() as d:
-        return (run(name, cmd + ["--output=%s" % d]) and
-                run(name, ["diff", "-ru", "-x.*", expected, d]))
+        return (run(lock, name, cmd + ["--output=%s" % d]) and
+                run(lock, name, ["diff", "-ru", "-x.*", expected, d]))
 
 
-def data_test(opts, name, args=[]):
-    return diff_test(name, ["out/cur/%s" % name] + args, "test/%s" % name)
+def data_test(opts, lock, name, args=[]):
+    return diff_test(lock, name, ["out/cur/%s" % name] + args, "test/%s" % name)
 
 
-def offscreen_test(opts, name, args=[]):
+def offscreen_test(opts, lock, name, args=[]):
     cmd = ["out/cur/offscreen", name]
     if opts.smoke:
         cmd.append("--text")
         expected = "test/smoke/%s" % name
     else:
         expected = "test/%s" % name
-    return diff_test(name, cmd + args, expected)
+    return diff_test(lock, name, cmd + args, expected)
 
 
-def replay_test(opts, name, args=[]):
+def replay_test(opts, lock, name, args=[]):
     cmd = ["out/cur/replay", "test/%s.NLRP" % name, "--text"]
     if opts.smoke:
         cmd.append("--smoke")
         expected = "test/smoke/%s" % name
     else:
         expected = "test/%s" % name
-    return diff_test(name, cmd + args, expected)
+    return diff_test(lock, name, cmd + args, expected)
 
 
 def call(args):
-    return args[0](*args[1:])
+    fn = args[0]
+    opts = args[1]
+    lock = args[2]
+    name = args[3]
+    args = list(*args[4:])
+
+    start = time.time()
+    result = fn(opts, lock, name, args)
+    end = time.time()
+
+    with lock:
+        if result:
+            rstr = "\033[1;32mPASSED\033[0m"
+        else:
+            rstr = "\033[1;31mFAILED\033[0m"
+        if len(name) > 36:
+            name = name[:33] + "..."
+        sys.stderr.write("  %-40s %s in %0.2fs\n" % (name, rstr, end - start))
+    return result
 
 
 def main():
@@ -65,40 +85,48 @@ def main():
     parser.add_argument("--smoke", action="store_true")
     opts = parser.parse_args()
 
+    lock = multiprocessing.Lock()
     pool = multiprocessing.pool.ThreadPool()
-    result = pool.map_async(call, [
-        (unit_test, opts, "fixed-test"),
+    tests = [
+        (unit_test, opts, lock, "fixed-test"),
 
-        (data_test, opts, "build-pix"),
-        (data_test, opts, "object-data"),
-        (data_test, opts, "shapes"),
-        (data_test, opts, "tint"),
+        (data_test, opts, lock, "build-pix"),
+        (data_test, opts, lock, "object-data"),
+        (data_test, opts, lock, "shapes"),
+        (data_test, opts, lock, "tint"),
 
-        (offscreen_test, opts, "main-screen"),
-        (offscreen_test, opts, "mission-briefing", ["--text"]),
-        (offscreen_test, opts, "options"),
-        (offscreen_test, opts, "pause", ["--text"]),
+        (offscreen_test, opts, lock, "main-screen"),
+        (offscreen_test, opts, lock, "mission-briefing", ["--text"]),
+        (offscreen_test, opts, lock, "options"),
+        (offscreen_test, opts, lock, "pause", ["--text"]),
 
-        (replay_test, opts, "and-it-feels-so-good"),
-        (replay_test, opts, "astrotrash-plus"),
-        (replay_test, opts, "blood-toil-tears-sweat"),
-        (replay_test, opts, "hand-over-fist"),
-        (replay_test, opts, "hornets-nest"),
-        (replay_test, opts, "make-way"),
-        (replay_test, opts, "moons-for-goons"),
-        (replay_test, opts, "out-of-the-frying-pan"),
-        (replay_test, opts, "shoplifter-1"),
-        (replay_test, opts, "space-race"),
-        (replay_test, opts, "the-left-hand"),
-        (replay_test, opts, "the-mothership-connection"),
-        (replay_test, opts, "the-stars-have-ears"),
-        (replay_test, opts, "while-the-iron-is-hot"),
-        (replay_test, opts, "yo-ho-ho"),
-        (replay_test, opts, "you-should-have-seen-the-one-that-got-away"),
-    ])
+        (replay_test, opts, lock, "and-it-feels-so-good"),
+        (replay_test, opts, lock, "astrotrash-plus"),
+        (replay_test, opts, lock, "blood-toil-tears-sweat"),
+        (replay_test, opts, lock, "hand-over-fist"),
+        (replay_test, opts, lock, "hornets-nest"),
+        (replay_test, opts, lock, "make-way"),
+        (replay_test, opts, lock, "moons-for-goons"),
+        (replay_test, opts, lock, "out-of-the-frying-pan"),
+        (replay_test, opts, lock, "shoplifter-1"),
+        (replay_test, opts, lock, "space-race"),
+        (replay_test, opts, lock, "the-left-hand"),
+        (replay_test, opts, lock, "the-mothership-connection"),
+        (replay_test, opts, lock, "the-stars-have-ears"),
+        (replay_test, opts, lock, "while-the-iron-is-hot"),
+        (replay_test, opts, lock, "yo-ho-ho"),
+        (replay_test, opts, lock, "you-should-have-seen-the-one-that-got-away"),
+    ]
+
+    sys.stderr.write("Running %d tests:\n" % len(tests))
+    start = time.time()
+    result = pool.map_async(call, tests)
     pool.close()
     result.wait()
-    if all(result.get()):
+    end = time.time()
+    results = result.get()
+    sys.stderr.write("\nRan %d tests in %.2fs\n" % (len(tests), end - start))
+    if all(results):
         sys.stderr.write("All tests passed!\n")
     else:
         sys.stderr.write("Some tests failed.\n")
