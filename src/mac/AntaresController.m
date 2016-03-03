41df5c788968313823eaed5a1e0a824653f838f2
Chris Pickel <sfiera@sfzmail.com>
2016-03-03 11:40:20 -0500

    Remove obsolute resolution-related code.

--- src/mac/AntaresController.m
+++ src/mac/AntaresController.m
@@ -108,107 +108,6 @@ static NSURL* url(const char* utf8_bytes) {
     }
 }
 
-- (void)setResolutionFrom:(NSMenuItem*)sender {
-    // Assumes that the sender's representedObject is a display mode
-    // dictionary, as set below.  Updates user defaults with the width
-    // and height of the selected resolution.
-    int width = CGDisplayModeGetWidth((CGDisplayModeRef)[sender representedObject]);
-    int height = CGDisplayModeGetHeight((CGDisplayModeRef)[sender representedObject]);;
-    [[NSUserDefaults standardUserDefaults] setInteger:width forKey:kScreenWidth];
-    [[NSUserDefaults standardUserDefaults] setInteger:height forKey:kScreenHeight];
-}
-
-- (void)updateResolutionList {
-    // When selecting a resolution, we'll pick the largest resolution we
-    // see that has (width <= user_width) and (height <= user_height).
-    // If the user has not yet selected a screen size, we default
-    // user_width and user_height to INT_MAX.
-    //
-    // This scheme has three important properties:
-    //
-    // * If the user has selected a resolution, and it is available,
-    //   then we will pick exactly that.
-    // * If the user has selected a resolution, but no longer has a
-    //   screen that big, then we will pick the largest resolution that
-    //   is still available.
-    // * If the user has never selected a resolution, then we will pick
-    //   the largest one available.
-    int user_width = INT_MAX;
-    int user_height = INT_MAX;
-    if ([[NSUserDefaults standardUserDefaults] objectForKey:kScreenWidth] != nil) {
-        user_width = [[NSUserDefaults standardUserDefaults] integerForKey:kScreenWidth];
-    }
-    if ([[NSUserDefaults standardUserDefaults] objectForKey:kScreenHeight] != nil) {
-        user_height = [[NSUserDefaults standardUserDefaults] integerForKey:kScreenHeight];
-    }
-    int best_resolution = -1;
-
-    [_resolution_list removeAllItems];
-    NSMutableArray* display_modes = [NSMutableArray
-        arrayWithArray:
-            [(NSArray*)CGDisplayCopyAllDisplayModes(kCGDirectMainDisplay, nil) autorelease]];
-    [display_modes sortUsingFunction:compare_resolutions context:nil];
-    int i;
-    for (i = 0; i < [display_modes count]; ++i) {
-        NSDictionary* display_mode = [display_modes objectAtIndex:i];
-        int width = CGDisplayModeGetWidth((CGDisplayModeRef)display_mode);
-        int height = CGDisplayModeGetHeight((CGDisplayModeRef)display_mode);
-
-        // Formatted string is as produced in the "Displays" pane in
-        // System Preferences.
-        NSString* title = [NSString stringWithFormat:@"%d × %d", width, height];
-
-        // We get multiple display modes with the same width and height,
-        // because display modes also encode color depth and maybe other
-        // things we're not interested in.  If 
-        if ([_resolution_list indexOfItemWithTitle:title] >= 0) {
-            continue;
-        }
-
-        // Use the display mode as the represented object of the menu
-        // item.  It has some extraneous information we're not
-        // interested in, but we've already got it and it has the width
-        // and height of the screen.
-        NSMenuItem* menu_item = [[[NSMenuItem alloc] initWithTitle:title action:nil
-            keyEquivalent:@""] autorelease];
-        [menu_item setRepresentedObject:display_mode];
-        [menu_item setTarget:self];
-        [menu_item setAction:@selector(setResolutionFrom:)];
-
-        if (best_resolution < 1) {
-            if ((height <= user_height) && (width <= user_width)) {
-                best_resolution = [_resolution_list numberOfItems];
-            }
-        } else {
-            [menu_item setEnabled:NO];
-        }
-
-        [[_resolution_list menu] addItem:menu_item];
-    }
-
-    // Select the best resolution from the list.  This doesn't trigger
-    // its action, so fire that off manually.  We care about doing so
-    // because, in the event that the user had an invalid preference or
-    // no preference, we want to use the best match as chosen above.
-    [_resolution_list selectItemAtIndex:best_resolution];
-    [self setResolutionFrom:[_resolution_list itemAtIndex:best_resolution]];
-
-    // TODO(sfiera): watch for resolution changes and update the list.
-}
-
-- (BOOL)validateMenuItem:(NSMenuItem*)item {
-    NSScreen* main = [NSScreen mainScreen];
-    NSSize size;
-    if (_window_checkbox.intValue) {
-        size = main.frame.size;
-    } else {
-        size = [main convertRectToBacking:main.frame].size;
-    }
-    CGDisplayModeRef mode = (CGDisplayModeRef)[item representedObject];
-    return (CGDisplayModeGetWidth(mode) <= size.width)
-        && (CGDisplayModeGetHeight(mode) <= size.height);
-}
-
 - (void)setScenarioFrom:(NSMenuItem*)sender {
     NSString* identifier = [[sender representedObject] objectForKey:kIdentifier];
     NSString* title = [[sender representedObject] objectForKey:kTitle];
@@ -280,10 +179,8 @@ static NSURL* url(const char* utf8_bytes) {
 }
 
 - (void)awakeFromNib {
-    [self updateResolutionList];
     [self updateScenarioList];
-    bool skip_settings = 
-        [[NSUserDefaults standardUserDefaults] boolForKey:kSkipSettings];
+    bool skip_settings = [[NSUserDefaults standardUserDefaults] boolForKey:kSkipSettings];
     [_skip_checkbox setIntValue:skip_settings];
 }
 
