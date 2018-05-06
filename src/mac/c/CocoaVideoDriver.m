1db9321ea34ca1cf10f90d364399a569ffe0afe9
Chris Pickel <sfiera@twotaled.com>
2018-05-06 16:55:16 +0200

    Add NSFullScreenWindowMask on NSWindow
    Possibly needed; see #218.

--- src/mac/c/CocoaVideoDriver.m
+++ src/mac/c/CocoaVideoDriver.m
@@ -70,7 +70,8 @@ AntaresWindow* antares_window_create(CGLPixelFormatObj pixel_format, CGLContextO
     window->context       = [[NSOpenGLContext alloc] initWithCGLContextObj:context];
 
     NSRect window_rect = NSMakeRect(0, 0, 640, 480);
-    int    style_mask  = NSTitledWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask;
+    int    style_mask  = NSTitledWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask |
+                     NSFullScreenWindowMask;
 
     window->view =
             [[NSOpenGLView alloc] initWithFrame:window_rect pixelFormat:window->pixel_format];
