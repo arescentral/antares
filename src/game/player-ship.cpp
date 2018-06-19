6bc94bbac1490756220fff244ffa90ae194b3d3d
Chris Pickel <sfiera@twotaled.com>
2018-06-19 23:18:57 +0200

    Don’t pass ship to ResetPlayerShip()
    Set g.ship outside of it, in the two places where it is called. It’s not
    really about setting g.ship, it’s about setting up other things.

--- src/game/player-ship.cpp
+++ src/game/player-ship.cpp
@@ -108,8 +108,7 @@ pn::string name_with_hot_key_suffix(Handle<SpaceObject> space_object) {
 
 }  // namespace
 
-void ResetPlayerShip(Handle<SpaceObject> which) {
-    g.ship          = which;
+void ResetPlayerShip() {
     g.control_label = Label::add(0, 0, 0, 10, SpaceObject::none(), true, Hue::YELLOW);
     g.target_label  = Label::add(0, 0, 0, -20, SpaceObject::none(), true, Hue::SKY_BLUE);
     g.send_label    = Label::add(200, 200, 0, 30, SpaceObject::none(), false, Hue::GREEN);
