85f39a42f79c6ba75cf098200e6e93c4e179df74
Chris Pickel <sfiera@twotaled.com>
2018-06-29 21:07:10 +0200

    Move ObjectRef to own header

---
+++ include/data/object-ref.hpp
@@ -0,0 +1,42 @@
+// Copyright (C) 1997, 1999-2001, 2008 Nathan Lamont
+// Copyright (C) 2008-2018 The Antares Authors
+//
+// This file is part of Antares, a tactical space combat game.
+//
+// Antares is free software: you can redistribute it and/or modify it
+// under the terms of the Lesser GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Antares is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with Antares.  If not, see http://www.gnu.org/licenses/
+
+#ifndef ANTARES_DATA_OBJECT_REF_HPP_
+#define ANTARES_DATA_OBJECT_REF_HPP_
+
+#include "data/handle.hpp"
+
+#include <sfz/sfz.hpp>
+
+namespace antares {
+
+struct Initial;
+class path_value;
+
+struct ObjectRef {
+    enum class Type { INITIAL, FLAGSHIP, CONTROL, TARGET } type;
+    Handle<const Initial> initial;
+    Handle<Admiral>       admiral;
+};
+
+ObjectRef                required_object_ref(path_value x);
+sfz::optional<ObjectRef> optional_object_ref(path_value x);
+
+}  // namespace antares
+
+#endif  // ANTARES_DATA_OBJECT_REF_HPP_
