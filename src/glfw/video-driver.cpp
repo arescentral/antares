7eee917ec2c800b53524cf70b619751dc3ff7360
Chris Pickel <sfiera@twotaled.com>
2020-03-09 10:52:29 +0900

    Just use a switch-case statement

--- src/glfw/video-driver.cpp
+++ src/glfw/video-driver.cpp
@@ -33,139 +33,129 @@ namespace antares {
 
 static const ticks kDoubleClickInterval = ticks(30);
 
-namespace {
+static Key glfw_key_to_usb(int key) {
+    switch (key) {
+        case GLFW_KEY_SPACE: return Key::SPACE;
+        case GLFW_KEY_APOSTROPHE: return Key::QUOTE;
+        case GLFW_KEY_COMMA: return Key::COMMA;
+        case GLFW_KEY_MINUS: return Key::MINUS;
+        case GLFW_KEY_PERIOD: return Key::PERIOD;
+        case GLFW_KEY_SLASH: return Key::SLASH;
 
-struct GLFWKeyMap {
-    Key map[GLFW_KEY_LAST + 1];
-};
+        case GLFW_KEY_0: return Key::K0;
+        case GLFW_KEY_1: return Key::K1;
+        case GLFW_KEY_2: return Key::K2;
+        case GLFW_KEY_3: return Key::K3;
+        case GLFW_KEY_4: return Key::K4;
+        case GLFW_KEY_5: return Key::K5;
+        case GLFW_KEY_6: return Key::K6;
+        case GLFW_KEY_7: return Key::K7;
+        case GLFW_KEY_8: return Key::K8;
+        case GLFW_KEY_9: return Key::K9;
 
-}  // namespace
+        case GLFW_KEY_SEMICOLON: return Key::SEMICOLON;
+        case GLFW_KEY_EQUAL: return Key::EQUALS;
 
-static GLFWKeyMap glfw_key_to_usb() {
-    GLFWKeyMap keys = {};
+        case GLFW_KEY_A: return Key::A;
+        case GLFW_KEY_B: return Key::B;
+        case GLFW_KEY_C: return Key::C;
+        case GLFW_KEY_D: return Key::D;
+        case GLFW_KEY_E: return Key::E;
+        case GLFW_KEY_F: return Key::F;
+        case GLFW_KEY_G: return Key::G;
+        case GLFW_KEY_H: return Key::H;
+        case GLFW_KEY_I: return Key::I;
+        case GLFW_KEY_J: return Key::J;
+        case GLFW_KEY_K: return Key::K;
+        case GLFW_KEY_L: return Key::L;
+        case GLFW_KEY_M: return Key::M;
+        case GLFW_KEY_N: return Key::N;
+        case GLFW_KEY_O: return Key::O;
+        case GLFW_KEY_P: return Key::P;
+        case GLFW_KEY_Q: return Key::Q;
+        case GLFW_KEY_R: return Key::R;
+        case GLFW_KEY_S: return Key::S;
+        case GLFW_KEY_T: return Key::T;
+        case GLFW_KEY_U: return Key::U;
+        case GLFW_KEY_V: return Key::V;
+        case GLFW_KEY_W: return Key::W;
+        case GLFW_KEY_X: return Key::X;
+        case GLFW_KEY_Y: return Key::Y;
+        case GLFW_KEY_Z: return Key::Z;
 
-    keys.map[GLFW_KEY_SPACE]      = Key::SPACE;
-    keys.map[GLFW_KEY_APOSTROPHE] = Key::QUOTE;
-    keys.map[GLFW_KEY_COMMA]      = Key::COMMA;
-    keys.map[GLFW_KEY_MINUS]      = Key::MINUS;
-    keys.map[GLFW_KEY_PERIOD]     = Key::PERIOD;
-    keys.map[GLFW_KEY_SLASH]      = Key::SLASH;
+        case GLFW_KEY_LEFT_BRACKET: return Key::L_BRACKET;
+        case GLFW_KEY_BACKSLASH: return Key::BACKSLASH;
+        case GLFW_KEY_RIGHT_BRACKET: return Key::R_BRACKET;
+        case GLFW_KEY_GRAVE_ACCENT: return Key::BACKTICK;
+        case GLFW_KEY_WORLD_1: return Key::NONE;
+        case GLFW_KEY_WORLD_2: return Key::NONE;
+        case GLFW_KEY_ESCAPE: return Key::ESCAPE;
+        case GLFW_KEY_ENTER: return Key::RETURN;
+        case GLFW_KEY_TAB: return Key::TAB;
+        case GLFW_KEY_BACKSPACE: return Key::BACKSPACE;
+        case GLFW_KEY_INSERT: return Key::NONE;
+        case GLFW_KEY_DELETE: return Key::DEL;
+        case GLFW_KEY_RIGHT: return Key::RIGHT_ARROW;
+        case GLFW_KEY_LEFT: return Key::LEFT_ARROW;
+        case GLFW_KEY_DOWN: return Key::DOWN_ARROW;
+        case GLFW_KEY_UP: return Key::UP_ARROW;
+        case GLFW_KEY_PAGE_UP: return Key::PAGE_UP;
+        case GLFW_KEY_PAGE_DOWN: return Key::PAGE_DOWN;
+        case GLFW_KEY_HOME: return Key::HOME;
+        case GLFW_KEY_END: return Key::END;
+        case GLFW_KEY_CAPS_LOCK: return Key::CAPS_LOCK;
+        case GLFW_KEY_SCROLL_LOCK: return Key::NONE;
+        case GLFW_KEY_NUM_LOCK: return Key::NONE;
+        case GLFW_KEY_PRINT_SCREEN: return Key::NONE;
+        case GLFW_KEY_PAUSE: return Key::NONE;
 
-    keys.map[GLFW_KEY_0] = Key::K0;
-    keys.map[GLFW_KEY_1] = Key::K1;
-    keys.map[GLFW_KEY_2] = Key::K2;
-    keys.map[GLFW_KEY_3] = Key::K3;
-    keys.map[GLFW_KEY_4] = Key::K4;
-    keys.map[GLFW_KEY_5] = Key::K5;
-    keys.map[GLFW_KEY_6] = Key::K6;
-    keys.map[GLFW_KEY_7] = Key::K7;
-    keys.map[GLFW_KEY_8] = Key::K8;
-    keys.map[GLFW_KEY_9] = Key::K9;
+        case GLFW_KEY_F1: return Key::F1;
+        case GLFW_KEY_F2: return Key::F2;
+        case GLFW_KEY_F3: return Key::F3;
+        case GLFW_KEY_F4: return Key::F4;
+        case GLFW_KEY_F5: return Key::F5;
+        case GLFW_KEY_F6: return Key::F6;
+        case GLFW_KEY_F7: return Key::F7;
+        case GLFW_KEY_F8: return Key::F8;
+        case GLFW_KEY_F9: return Key::F9;
+        case GLFW_KEY_F10: return Key::F10;
+        case GLFW_KEY_F11: return Key::F11;
+        case GLFW_KEY_F12: return Key::F12;
+        case GLFW_KEY_F13: return Key::F13;
+        case GLFW_KEY_F14: return Key::F14;
+        case GLFW_KEY_F15: return Key::F15;
 
-    keys.map[GLFW_KEY_SEMICOLON] = Key::SEMICOLON;
-    keys.map[GLFW_KEY_EQUAL]     = Key::EQUALS;
+        case GLFW_KEY_KP_1: return Key::N1;
+        case GLFW_KEY_KP_2: return Key::N2;
+        case GLFW_KEY_KP_3: return Key::N3;
+        case GLFW_KEY_KP_4: return Key::N4;
+        case GLFW_KEY_KP_5: return Key::N5;
+        case GLFW_KEY_KP_6: return Key::N6;
+        case GLFW_KEY_KP_7: return Key::N7;
+        case GLFW_KEY_KP_8: return Key::N8;
+        case GLFW_KEY_KP_9: return Key::N9;
 
-    keys.map[GLFW_KEY_A] = Key::A;
-    keys.map[GLFW_KEY_B] = Key::B;
-    keys.map[GLFW_KEY_C] = Key::C;
-    keys.map[GLFW_KEY_D] = Key::D;
-    keys.map[GLFW_KEY_E] = Key::E;
-    keys.map[GLFW_KEY_F] = Key::F;
-    keys.map[GLFW_KEY_G] = Key::G;
-    keys.map[GLFW_KEY_H] = Key::H;
-    keys.map[GLFW_KEY_I] = Key::I;
-    keys.map[GLFW_KEY_J] = Key::J;
-    keys.map[GLFW_KEY_K] = Key::K;
-    keys.map[GLFW_KEY_L] = Key::L;
-    keys.map[GLFW_KEY_M] = Key::M;
-    keys.map[GLFW_KEY_N] = Key::N;
-    keys.map[GLFW_KEY_O] = Key::O;
-    keys.map[GLFW_KEY_P] = Key::P;
-    keys.map[GLFW_KEY_Q] = Key::Q;
-    keys.map[GLFW_KEY_R] = Key::R;
-    keys.map[GLFW_KEY_S] = Key::S;
-    keys.map[GLFW_KEY_T] = Key::T;
-    keys.map[GLFW_KEY_U] = Key::U;
-    keys.map[GLFW_KEY_V] = Key::V;
-    keys.map[GLFW_KEY_W] = Key::W;
-    keys.map[GLFW_KEY_X] = Key::X;
-    keys.map[GLFW_KEY_Y] = Key::Y;
-    keys.map[GLFW_KEY_Z] = Key::Z;
+        case GLFW_KEY_KP_DECIMAL: return Key::N_PERIOD;
+        case GLFW_KEY_KP_DIVIDE: return Key::N_DIVIDE;
+        case GLFW_KEY_KP_MULTIPLY: return Key::N_TIMES;
+        case GLFW_KEY_KP_SUBTRACT: return Key::N_MINUS;
+        case GLFW_KEY_KP_ADD: return Key::N_PLUS;
+        case GLFW_KEY_KP_ENTER: return Key::N_ENTER;
+        case GLFW_KEY_KP_EQUAL: return Key::N_EQUALS;
 
-    keys.map[GLFW_KEY_LEFT_BRACKET]  = Key::L_BRACKET;
-    keys.map[GLFW_KEY_BACKSLASH]     = Key::BACKSLASH;
-    keys.map[GLFW_KEY_RIGHT_BRACKET] = Key::R_BRACKET;
-    keys.map[GLFW_KEY_GRAVE_ACCENT]  = Key::BACKTICK;
-    keys.map[GLFW_KEY_WORLD_1]       = Key::NONE;
-    keys.map[GLFW_KEY_WORLD_2]       = Key::NONE;
-    keys.map[GLFW_KEY_ESCAPE]        = Key::ESCAPE;
-    keys.map[GLFW_KEY_ENTER]         = Key::RETURN;
-    keys.map[GLFW_KEY_TAB]           = Key::TAB;
-    keys.map[GLFW_KEY_BACKSPACE]     = Key::BACKSPACE;
-    keys.map[GLFW_KEY_INSERT]        = Key::NONE;
-    keys.map[GLFW_KEY_DELETE]        = Key::DEL;
-    keys.map[GLFW_KEY_RIGHT]         = Key::RIGHT_ARROW;
-    keys.map[GLFW_KEY_LEFT]          = Key::LEFT_ARROW;
-    keys.map[GLFW_KEY_DOWN]          = Key::DOWN_ARROW;
-    keys.map[GLFW_KEY_UP]            = Key::UP_ARROW;
-    keys.map[GLFW_KEY_PAGE_UP]       = Key::PAGE_UP;
-    keys.map[GLFW_KEY_PAGE_DOWN]     = Key::PAGE_DOWN;
-    keys.map[GLFW_KEY_HOME]          = Key::HOME;
-    keys.map[GLFW_KEY_END]           = Key::END;
-    keys.map[GLFW_KEY_CAPS_LOCK]     = Key::CAPS_LOCK;
-    keys.map[GLFW_KEY_SCROLL_LOCK]   = Key::NONE;
-    keys.map[GLFW_KEY_NUM_LOCK]      = Key::NONE;
-    keys.map[GLFW_KEY_PRINT_SCREEN]  = Key::NONE;
-    keys.map[GLFW_KEY_PAUSE]         = Key::NONE;
+        case GLFW_KEY_LEFT_SHIFT: return Key::SHIFT;
+        case GLFW_KEY_LEFT_CONTROL: return Key::CONTROL;
+        case GLFW_KEY_LEFT_ALT: return Key::OPTION;
+        case GLFW_KEY_LEFT_SUPER: return Key::COMMAND;
+        case GLFW_KEY_RIGHT_SHIFT: return Key::R_SHIFT;
+        case GLFW_KEY_RIGHT_CONTROL: return Key::R_CONTROL;
+        case GLFW_KEY_RIGHT_ALT: return Key::R_OPTION;
+        case GLFW_KEY_RIGHT_SUPER: return Key::R_COMMAND;
+        case GLFW_KEY_MENU: return Key::NONE;
 
-    keys.map[GLFW_KEY_F1]  = Key::F1;
-    keys.map[GLFW_KEY_F2]  = Key::F2;
-    keys.map[GLFW_KEY_F3]  = Key::F3;
-    keys.map[GLFW_KEY_F4]  = Key::F4;
-    keys.map[GLFW_KEY_F5]  = Key::F5;
-    keys.map[GLFW_KEY_F6]  = Key::F6;
-    keys.map[GLFW_KEY_F7]  = Key::F7;
-    keys.map[GLFW_KEY_F8]  = Key::F8;
-    keys.map[GLFW_KEY_F9]  = Key::F9;
-    keys.map[GLFW_KEY_F10] = Key::F10;
-    keys.map[GLFW_KEY_F11] = Key::F11;
-    keys.map[GLFW_KEY_F12] = Key::F12;
-    keys.map[GLFW_KEY_F13] = Key::F13;
-    keys.map[GLFW_KEY_F14] = Key::F14;
-    keys.map[GLFW_KEY_F15] = Key::F15;
-
-    keys.map[GLFW_KEY_KP_1] = Key::N1;
-    keys.map[GLFW_KEY_KP_2] = Key::N2;
-    keys.map[GLFW_KEY_KP_3] = Key::N3;
-    keys.map[GLFW_KEY_KP_4] = Key::N4;
-    keys.map[GLFW_KEY_KP_5] = Key::N5;
-    keys.map[GLFW_KEY_KP_6] = Key::N6;
-    keys.map[GLFW_KEY_KP_7] = Key::N7;
-    keys.map[GLFW_KEY_KP_8] = Key::N8;
-    keys.map[GLFW_KEY_KP_9] = Key::N9;
-
-    keys.map[GLFW_KEY_KP_DECIMAL]  = Key::N_PERIOD;
-    keys.map[GLFW_KEY_KP_DIVIDE]   = Key::N_DIVIDE;
-    keys.map[GLFW_KEY_KP_MULTIPLY] = Key::N_TIMES;
-    keys.map[GLFW_KEY_KP_SUBTRACT] = Key::N_MINUS;
-    keys.map[GLFW_KEY_KP_ADD]      = Key::N_PLUS;
-    keys.map[GLFW_KEY_KP_ENTER]    = Key::N_ENTER;
-    keys.map[GLFW_KEY_KP_EQUAL]    = Key::N_EQUALS;
-
-    keys.map[GLFW_KEY_LEFT_SHIFT]    = Key::SHIFT;
-    keys.map[GLFW_KEY_LEFT_CONTROL]  = Key::CONTROL;
-    keys.map[GLFW_KEY_LEFT_ALT]      = Key::OPTION;
-    keys.map[GLFW_KEY_LEFT_SUPER]    = Key::COMMAND;
-    keys.map[GLFW_KEY_RIGHT_SHIFT]   = Key::R_SHIFT;
-    keys.map[GLFW_KEY_RIGHT_CONTROL] = Key::R_CONTROL;
-    keys.map[GLFW_KEY_RIGHT_ALT]     = Key::R_OPTION;
-    keys.map[GLFW_KEY_RIGHT_SUPER]   = Key::R_COMMAND;
-    keys.map[GLFW_KEY_MENU]          = Key::NONE;
-
-    return keys;
+        default: return Key::NONE;
+    }
 }
-
-static GLFWKeyMap kGLFWKeyToUSB = glfw_key_to_usb();
 
 static void throw_error(int code, const char* message) {
     throw std::runtime_error(pn::format("{0}: {1}", code, message).c_str());
@@ -210,7 +200,7 @@ void GLFWVideoDriver::key(int key, int scancode, int action, int mods) {
     if (key < 0) {
         return;
     }
-    Key usb_key = kGLFWKeyToUSB.map[key];
+    Key usb_key = glfw_key_to_usb(key);
     if (usb_key == Key::NONE) {
         return;
     }
