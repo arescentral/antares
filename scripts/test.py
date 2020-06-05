08fb6f297ee6027ed167ec1d43a7c46a5e11f8bf
Chris Pickel <sfiera@twotaled.com>
2020-06-05 20:29:25 +0900

    Fix scripts/test.py cStringIO for python3

--- scripts/test.py
+++ scripts/test.py
@@ -7,7 +7,7 @@
 import argparse
 import collections
 import contextlib
-import cStringIO
+import io
 import multiprocessing.pool
 import os
 import shutil
@@ -90,7 +90,7 @@ def call(args):
     name = args[3]
     args = list(args[4:])
 
-    sys.stdout = cStringIO.StringIO()
+    sys.stdout = io.StringIO()
 
     queue.put((
         name,
