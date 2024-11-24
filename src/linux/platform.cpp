1f03f801ba0f0b48c7051d86fe8acf53f66e6c69
Chris Pickel <sfiera@twotaled.com>
2024-11-24 16:12:07 +0900

    Add platform_name constant

---
+++ src/linux/platform.cpp
@@ -0,0 +1,25 @@
+// Copyright (C) 1997, 1999-2001, 2008 Nathan Lamont
+// Copyright (C) 2024 The Antares Authors
+//
+// This file is part of Antares, a tactical space combat game.
+//
+// Antares is free software: you can redistribute it and/or modify it
+// under the terms of the Lesser GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Antares is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with Antares.  If not, see http://www.gnu.org/licenses/
+
+#include "config/platform.hpp"
+
+namespace antares {
+
+pn::string_view platform_name = "linux";
+
+}  // namespace antares
